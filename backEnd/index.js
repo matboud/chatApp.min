f459e96641e7cd77af51331de41cecb090aa4d1f
Matboud <m.matboud@gmail.com>
2020-05-23 16:36:13 +0000

    emitting sent message and displaying it in app

--- backEnd/index.js
+++ backEnd/index.js
@@ -6,6 +6,10 @@ const port = 3000;
 
 io.on("connection", socket => {
    console.log("user in");
+   socket.on('chat message', msg => {
+      console.log('message sent: ', msg)
+      io.emit('chat message', msg)
+   })
 });
 
 server.listen(port, () => console.log("server ON!" + port));
--- mobile/chatApp/App.js
+++ mobile/chatApp/App.js
@@ -1,114 +1,107 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+   SafeAreaView,
+   View,
+   Text,
+   TextInput,
+   YellowBox,
+   TouchableOpacity
 } from 'react-native';
+import io from 'socket.io-client';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+class App extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         text: '',
+         chatMessages: []
+      }
+   }
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+   componentDidMount() {
+      this.socket = io("http://localhost:3000");
+      // listener to messages
+      this.socket.on('chat message', msg => {
+         this.setState({
+            chatMessages: [...this.state.chatMessages, msg]
+         })
+      })
+   }
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+   onChangeText = (text) => {
+      this.setState({ text })
+   }
 
-export default App;
+   handleSubmit = () => {
+      this.socket.emit('chat message', this.state.text);
+      this.setState({ text: '' })
+   }
+
+   render() {
+      YellowBox.ignoreWarnings([
+         'Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`, `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`. Did you mean to put these under `headers`?'
+      ])
+      return (
+         <SafeAreaView style={{ flex: 1, paddingBottom: 10 }}>
+            <View style={{ flex: 1, backgroundColor: '#ecf0f1', paddingVertical: 30, paddingHorizontal: 5 }}>
+               {
+                  this.state.chatMessages.map((message, index) => (
+                     <View key={index} style={{alignItems: 'flex-start', marginBottom: 5}}>
+                        <View style={{
+                           backgroundColor: '#3498db',
+                           paddingVertical: 15,
+                           paddingHorizontal: 20,
+                           borderTopLeftRadius: 0,
+                           borderTopRightRadius: 20,
+                           borderBottomLeftRadius: 20,
+                           borderBottomRightRadius: 20,
+                        }}>
+                           <Text style={{color: 'white'}}>{message}</Text>
+                        </View>
+
+                     </View>
+
+                  ))
+               }
+            </View>
+
+            <View style={{ 
+               paddingVertical: 20, 
+               backgroundColor: 'white', 
+               paddingHorizontal: 15,
+               borderTopWidth: 1,
+               borderTopColor: '#bdc3c7'
+            }}>
+               <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <View style={{ flex: 1, paddingRight: 15 }}>
+                     <TextInput
+                        style={{ height: 40, borderColor: 'gray', borderBottomWidth: 1 }}
+                        onChangeText={text => this.onChangeText(text)}
+                        value={this.state.text}
+                        placeholder="Message.."
+                        autoCorrect={false}
+                        onSubmitEditing={() => {
+                           this.handleSubmit()
+                        }}
+                     />
+                  </View>
+                  <TouchableOpacity>
+                     <View style={{
+                        backgroundColor: '#3498db',
+                        width: 70,
+                        height: 50,
+                        borderRadius: 15,
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                     }}>
+                        <Text style={{ color: 'white' }}>Send</Text>
+                     </View>
+                  </TouchableOpacity>
+               </View>
+            </View>
+         </SafeAreaView>
+      )
+   }
+}
+
+export default App
